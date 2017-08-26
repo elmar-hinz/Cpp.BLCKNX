e84361fca371439641a07ffa94abaf3cc51a8081
Elmar Hinz <t3elmar@gmail.com>
2017-08-26 13:54:05 +0200

    [TASK] Remove matrix from AlignmentScoreMeasurer
    Remove the matrix and all related options, but remember we have this
    code. Just checkout the previous versions of the files.

--- Challenges/LOCA/Loca.cpp
+++ Challenges/LOCA/Loca.cpp
@@ -12,25 +12,25 @@ namespace blcknx {
     }
 
     void Loca::calc() {
-        provider.setGapPenalty(-5);
-        measurer.setScoreProvider(&provider);
-        measurer.setStrand1(model[0]);
-        measurer.setStrand2(model[1]);
-        measurer.enableMatrix();
-        measurer.enableFreeRides();
-        measurer.measure();
-        AlignmentScoreMeasurer::MatrixCell winner = measurer.findMatrixWinner();
-        AlignmentScoreMeasurer::Backtracks bt = measurer.backtrack(winner);
-        result.score = winner.score;
-        result.motif1 = bt.strand1;
-        result.motif2 = bt.strand2;
+//        provider.setGapPenalty(-5);
+//        measurer.setScoreProvider(&provider);
+//        measurer.setStrand1(model[0]);
+//        measurer.setStrand2(model[1]);
+//        measurer.enableMatrix();
+//        measurer.enableFreeRides();
+//        measurer.measure();
+//        AlignmentScoreMeasurer::MatrixCell winner = measurer.findMatrixWinner();
+//        AlignmentScoreMeasurer::Backtracks bt = measurer.backtrack(winner);
+//        result.score = winner.score;
+//        result.motif1 = bt.strand1;
+//        result.motif2 = bt.strand2;
 
     }
 
     void Loca::format() {
-        output += std::to_string(result.score) + "\n";
-        output += result.motif1 + "\n";
-        output += result.motif2;
+//        output += std::to_string(result.score) + "\n";
+//        output += result.motif1 + "\n";
+//        output += result.motif2;
     }
     
 }
--- Libraries/Blcknx/include/AlignmentScoreMeasurer.h
+++ Libraries/Blcknx/include/AlignmentScoreMeasurer.h
@@ -7,88 +7,38 @@
 
 #include <vector>
 #include <string>
+#include "AlignmentScoreProviderInterface.h"
 
 namespace blcknx {
-    class AlignmentScoreProviderInterface;
-
     class AlignmentScoreMeasurer {
     public:
-        struct MatrixCell {
-            std::pair<long, long> address;
-            std::pair<long, long> backPointer;
-            long score;
-
-            bool operator==(const MatrixCell &other) const {
-                return (
-                        score == other.score
-                        && backPointer == other.backPointer
-                        && address == other.address
-                );
-            }
-        };
-
-        struct Backtracks {
-            std::string common = "";
-            std::string strand1 = "";
-            std::string strand2 = "";
-            std::string alignment1 = "";
-            std::string alignment2 = "";
-        };
+        AlignmentScoreProviderInterface *getScoreProvider() const;
 
         void setScoreProvider(AlignmentScoreProviderInterface *provider);
 
-        AlignmentScoreProviderInterface *getScoreProvider() const;
-
-        void enableMatrix();
-
-        void disableMatrix();
-
-        bool hasMatrix();
-
-        void enableFreeRides();
-
-        void disableFreeRides();
-
-        bool hasFreeRides();
-
         void setStrand1(std::string strand1);
-
-        std::string getStrand1();
 
         void setStrand2(std::string strand2);
 
+        std::string getStrand1();
+
         std::string getStrand2();
-
-        void measure();
-
-        long measure(std::string strand1, std::string strand2);
-
-        MatrixCell findMatrixWinner();
-
-        Backtracks backtrack(MatrixCell endPoint);
 
         std::vector<long> getFront();
 
         long getScore();
 
-        std::string debugMatrix();
-        std::string debugMatrixCell(MatrixCell cell);
+        void measure();
+
+        long measure(std::string strand1, std::string strand2);
 
     protected:
         AlignmentScoreProviderInterface *provider;
         std::string strand1;
         std::string strand2;
         std::vector<long> front;
-        std::vector<std::vector<MatrixCell>> matrix;
-        std::vector<std::vector<MatrixCell>> *matrixPointer;
-        bool freeRides = false;
-
-        void setupMatrix();
-        bool neighboursOrigin(MatrixCell cell);
 
     };
 }
-
-#include "AlignmentScoreProviderInterface.h"
 
 #endif //BLCKNX_ALIGNMENTSCOREMEASURER_H
